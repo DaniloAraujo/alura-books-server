73f01683ccae68cffc0ae3954ca253e882ebc366
Danilo Godinho <danilo.godinho@onebox.one>
2023-12-05 16:54:56 -0300

    feat: added lib cors for removed cors error in frontend project

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require("express")
+const express = require('express')
+const cors = require('cors')
 const routerBooks = require('./routes/books.js')
 
 const app = express()
 app.use(express.json())
+app.use(cors({origin: '*'}))
 
 const port = 8000
 
